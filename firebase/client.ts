7bd3d8c8f790cd1de00f540cc93a1c9c6b9a11b4
Cynthia Umwali <cynthiaumwali3@gmail.com>
2025-04-11 15:22:49 +0200

    Fix Firebase Firestore import and update package dependencies

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,6 +1,6 @@
 import {initializeApp, getApp, getApps} from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase-admin/firestore';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
